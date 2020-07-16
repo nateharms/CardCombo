4d65a250d726d0a2fbc9c8a5772feb93905c3d06
Nate Harms <harms.n@husky.neu.edu>
2020-07-16 14:05:41 -0400

    updated app to use bootstrap

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, request, redirect
+from flask_bootstrap import Bootstrap
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib
 from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
 import pandas as pd
-import time
+import time, os
+import pickle
 
 
-app = Flask(__name__)
+
+cards =  pickle.load(open('rewards.pkl', 'rb'))
+
+app = Flask(__name__, static_folder='templates/images')
+Bootstrap(app)
+
+app.vars = {}
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if (request.method == 'GET'):
-        print(request)
-    return render_template('base.html')
+    if (request.method == 'GET' and len(request.args) != 0):
+        return(plots())
+    print(list(cards.index)[:5])
+
+    return render_template('base.html', cards=list(cards.index))
 
 
 @app.route('/loading', methods=['GET', 'POST'])
@@ -23,9 +33,26 @@ def loading():
 
 @app.route('/results', methods=['GET', 'POST'])
 def plots():
-    render_template('loading.html')
-    time.sleep(5)
+    print([i for i in request.args.items()])
+    if (request.method == 'GET' and len(request.args) != 0):
+        print(app.vars)
     print('Loading...')
+    try:
+        flights = request.args['flights']
+        #flight_freq = 
+    
+    except:
+        return redirect('/')
+        
     return render_template('plots.html')
+
+@app.route('/database')
+def database():
+    cards = []
+    for card in sorted(os.listdir('templates/images')):
+        cards.append(card.strip('.svg'))
+
+    return render_template('database.html',  cards = cards )
+
 
 if __name__ == '__main__':app.run(port=8000)
