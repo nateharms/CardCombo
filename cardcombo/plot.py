b7ea8de1f1beeb8395e7dce988b3da51c53b7506
Nate Harms <harms.n@husky.neu.edu>
2020-07-22 19:57:21 -0400

    major overhaul to plotting

--- cardcombo/plot.py
+++ cardcombo/plot.py
@@ -1,113 +1,101 @@
 import os, pickle
-from scipy.interpolate import interp1d
-import matplotlib.pyplot as plt
+import shutil
 import numpy as np
 import pandas as pd
-import seaborn as sns
-sns.set()
-sns.set_style('whitegrid')
-sns.set_context('talk')
+import plotly.express as px
+import plotly.graph_objects as go
 
-import pickle, os
+# Not sure if we need
+from scipy.interpolate import interp1d
 from collections import Counter
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from PIL import Image
-from wordcloud import WordCloud
-sns.set()
-sns.set_style('whitegrid')
-sns.set_context('talk')
 
 
-def read_rewards():
-    # To be replaced with a formal database
-    f = open('calculated_rewards.pkl', 'rb')
-    calculated_rewards = pickle.load(f)
-    return calculated_rewards
 
-def plot_num_cards(calculated_rewards):
-    mins = []
-    medians = []
-    maxes = []
-    for card_number, rewards in calculated_rewards.items():
-        # For `card_number` number of credit cards ...
-        mins.append(np.min(rewards)) # ... the min rewards 
-        maxes.append(np.max(rewards)) # ... the max rewards
-        medians.append(np.median(rewards)) # ... the median rewards
+def radar_plot(series_list, save_name=None, save_svg=False, save_html=False, save_dir='.'):
+    
+    if not isinstance(series_list, list):
+        series_list = [series_list]
+    
+    fig = go.Figure()
+    for series in series_list:
+        fig.add_trace(go.Scatterpolar(
+            r = series[['flights', 'hotel', 'grocery', 'gas', 'dining', 'other']].values, 
+            name=series.name, 
+            theta=['Flights', 'Hotel', 'Grocery', 'Gas', 'Dining', 'Other'], 
+            fill='toself', opacity=0.3))
 
-    fig, ax = plt.subplots(figsize=(12,8))
+    if len(series_list) == 1:
+        name = series_list[0].name
+        fig.update_layout(title_text=name, 
+                  title_x=0.5, 
+                  polar = dict(radialaxis = dict(range=[0, 6])))
+    else:
+        name = 'credit_rewards_profile'
+        fig.update_layout(title_text='Credit Rewards Profile: {} Cards'.format(len(series_list)), 
+                  title_x=0.5, 
+                  polar = dict(radialaxis = dict(range=[0, 6])))
+    if save_name:
+        name = save_name
+    if save_html:
+        with open(f'{os.path.join(save_dir, name)}.html', 'w') as f:
+            f.write(fig.to_html())
 
-    ax.scatter(calculated_rewards.keys(), medians, label='Median')
+    if save_svg:
+        with open(f'{os.path.join(save_dir, name)}.svg', 'wb') as f:
+            f.write(fig.to_image('svg'))
 
-    smooth_median = max(medians)
-    smooth_number_of_cards = np.linspace(min(calculated_rewards.keys()), max(calculated_rewards.keys()), 500)  
-
-    # Smoothing the curve for min rewards 
-    power_smooth = interp1d(list(calculated_rewards.keys()), mins, kind='cubic')
-    smooth_min = max(power_smooth(smooth_number_of_cards)) # the limit
-    ax.plot(smooth_number_of_cards, power_smooth(smooth_number_of_cards), '-', c='g', linewidth=5, label='Maximum / Minimum')
-
-    # Smoothing the curve for max rewards
-    power_smooth = interp1d(list(calculated_rewards.keys()), maxes, kind='cubic')
-    smooth_max = max(power_smooth(smooth_number_of_cards)) # the limit
-    ax.plot(smooth_number_of_cards, power_smooth(smooth_number_of_cards), '-', c='g', linewidth=5)
-
-
-    # Plotting lines for the limits of the max, median, and min
-    ax.plot([0,10], [smooth_min, smooth_min], alpha=0.5, c='k', linewidth=3, label='Plateaus')
-    ax.plot([0,10], [smooth_max, smooth_max], alpha=0.5, c='k', linewidth=3)
-    ax.plot([0,10], [smooth_median, smooth_median], alpha=0.5, c='k', linewidth=3)
-    ax.annotate(f'Max plateau at ~${int(round(smooth_max,-1))}', xy=(6, smooth_max),  xycoords='data',
-                xytext=(0.8, 0.5), textcoords='axes fraction',
-                arrowprops=dict(facecolor='black', shrink=0.05),
-                horizontalalignment='right', verticalalignment='top',
-                )
-    ax.annotate(f'Min plateau at ~${int(round(smooth_min,-1))}', xy=(2, smooth_min),  xycoords='data',
-                xytext=(0.5, 0.3), textcoords='axes fraction',
-                arrowprops=dict(facecolor='black', shrink=0.05),
-                horizontalalignment='right', verticalalignment='top',
-                )
-
-    ax.annotate(f'Median plateau at ~${int(round(smooth_median,-1))}', xy=(4.5, smooth_median),  xycoords='data',
-                xytext=(0.7, 0.4), textcoords='axes fraction',
-                arrowprops=dict(facecolor='black', shrink=0.05),
-                horizontalalignment='right', verticalalignment='top',
-                )
-
-    ax.set_yscale('log', nonposy='clip')
-    ax.legend()
-    ax.set_xticks(range(11))
-    ax.set_ylabel('Annual Rewards (\$)')
-    ax.set_xlabel('Number of Credit Cards')
-    ax.set_title('Calculated rewards for various numbers of credit cards')
+    return fig
 
 
-def make_word_cloud():
-    number_of_cards = 2
-    results = word_cloud[number_of_cards]
-    scores = []
-    for (score, cards) in results:
-        # Cards are going to be `scored`
-        # Essentially, every dollar that was part of their a reward 
-        # they were a part of, counts towards their score and is summated
-        # across all samples
-        score = int(round(score, 0))
-        for card in cards:
-            scores += [card] * score
 
-    word_could_dict = Counter(scores) # Counting the number of points
+def plot_num_of_cards(results):
+    
+    df = pd.DataFrame(results)
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=df.columns, y=df.max(),
+                   mode='lines',
+                   name='Maximum Rewards'))
+    fig.add_trace(
+        go.Scatter(x=df.columns, y=df.median(),
+                   mode='markers',
+                   name='Median Rewards'))
+    fig.add_trace(
+        go.Scatter(x=df.columns, y=df.min(),
+                   mode='lines', name='Minimum Rewards'))
 
-    # Making a plot
-    wordcloud = WordCloud(width = 1500, 
-                        height = 800, 
-                        background_color='white',
-                        min_font_size=1,
-                        max_font_size=150,
-                        colormap='copper',
-                        ).generate_from_frequencies(word_could_dict)
-    fig = plt.figure(figsize=(15,8))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    fig.savefig('cloud.pdf', bbox_inches='tight',dpi=250)
+    for col in df.columns:
+        if col == 0:
+            showlegend = True
+            fig.add_trace(go.Violin(x=[col]*df[col].shape[0],
+                                    y=df[col],
+                                    opacity=0.5,
+                                    fillcolor='black',
+                                    line_color='black',
+                                    hoverinfo='none',
+                                    name='Distribution of Simulated Combinations',
+                                    showlegend=True
+                                    ))
+
+        else:
+            fig.add_trace(go.Violin(x=[col]*df[col].shape[0],
+                                    y=df[col],
+                                    opacity=0.5,
+                                    fillcolor='black',
+                                    line_color='black',
+                                    hoverinfo='none',
+                                    showlegend=False
+                                    ))
+
+
+    fig.update_layout(
+        title="Custom Credit Card Profile",
+        xaxis_title="Number of Credit Cards",
+        yaxis_title="Annual Rewards Earned ($)",
+        legend_title="Legend Title",
+        hovermode='x'
+    )
+
+
+    return fig
+
